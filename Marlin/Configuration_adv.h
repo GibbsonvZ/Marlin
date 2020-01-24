3324899e9ee09f4dad52ef12f84d9899eaff749d
Clemens <clemens.geppner@hotmail.com>
2020-01-24 19:12:49 +0100

    defined Z2 and Z2 current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1920,7 +1920,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z2)
-    #define Z2_CURRENT      800
+    #define Z2_CURRENT      760
     #define Z2_CURRENT_HOME Z2_CURRENT
     #define Z2_MICROSTEPS    16
     #define Z2_RSENSE         0.11
